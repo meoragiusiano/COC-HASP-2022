27db04220ef718a4600b853bc726bd30ba5393bd
Meora <87042984+meoragiusiano@users.noreply.github.com>
2022-07-27 11:10:05 -0500

    Update HASP2022_Temp.cpp

--- HASP2022_Main/HASP2022_Temp.cpp
+++ HASP2022_Main/HASP2022_Temp.cpp
@@ -7,7 +7,7 @@ extern long ThermResistance;
 
 long ReadTemp(int tempType) {
   long temp = 0;
-  
+
   if (tempType == 0)
     temp = GetTemp(PINS_TEMP_INSIDE);
   else if (tempType == 1)
@@ -18,19 +18,11 @@ long ReadTemp(int tempType) {
 
 //"GetTemp" adaptation from: https://www.circuitbasics.com/arduino-thermistor-temperature-sensor-tutorial/
 long GetTemp(int PINS[2]) {
-  int Vo;
-  float logR2, R2, T;
-  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
-  long tempSum = 0;
-  
+  int v_cum = 0;
   for (int i = 0; i < 2; i++)
   {
-    Vo = analogRead(PINS[i]);
-    R2 = ThermResistance * (1023.0 / (float)Vo - 1.0);
-    logR2 = log(R2);
-    T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
-    tempSum += T;
+    v_cum += analogRead(PINS[i]);
   }
 
-  return tempSum / 2;
+  return v_cum / 2.0;
 }
